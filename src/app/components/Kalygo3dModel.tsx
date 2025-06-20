95b8fbde6a67ae6c57dc5331a15ecfb526dba86c
thaddavis <thaddadavis@gmail.com>
2025-06-20 13:48:35 -0400

    fix typescript errors

--- src/app/components/Kalygo3dModel.tsx
+++ src/app/components/Kalygo3dModel.tsx
@@ -1,27 +1,30 @@
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 interface P {
   isMobile?: boolean;
 }
 
 export function Kalygo3dModel(props: P) {
-  // @ts-ignore
   const { nodes, materials } = useGLTF("/3d_kalygo.glb");
-  const ref = useRef<any>();
-  // @ts-ignore
-  useFrame((state, delta) => (ref.current.rotation.z += delta));
+  const ref = useRef<THREE.Mesh>(null);
+  
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.z += delta;
+    }
+  });
 
   return (
     <group {...props} dispose={null}>
       <mesh
-        // @ts-ignore
         ref={ref}
         castShadow
         receiveShadow
-        geometry={nodes.Curve.geometry}
-        material={materials["SVGMat.001"]}
+        geometry={(nodes.Curve as THREE.Mesh).geometry}
+        material={materials["SVGMat.001"] as THREE.Material}
         rotation={[Math.PI / 2, 0, 0]}
         scale={32}
       />
