c20a576af68c2bec0d59ae065eab363be16bfef0
thaddavis <thaddadavis@gmail.com>
2025-06-22 18:21:30 -0400

    Update Wagme config.

--- src/app/dapp/wagmiConfig.ts
+++ src/app/dapp/wagmiConfig.ts
@@ -2,12 +2,11 @@ import { http, createConfig } from 'wagmi'
 import { base, baseSepolia, mainnet, sepolia } from 'wagmi/chains'
 
 export const config = createConfig({
-  chains: [mainnet, sepolia],
+  chains: [mainnet, sepolia, base, baseSepolia],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
     [baseSepolia.id]: http(),
     [base.id]: http(),
-  },
-  ssr: true,
+  }
 })
