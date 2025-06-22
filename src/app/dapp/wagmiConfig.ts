2003c3d1123847a8e72fffc32dd1c1583024fffa
thaddavis <thaddadavis@gmail.com>
2025-06-22 18:15:54 -0400

    save config

--- src/app/dapp/wagmiConfig.ts
+++ src/app/dapp/wagmiConfig.ts
@@ -1,11 +1,13 @@
 import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { base, baseSepolia, mainnet, sepolia } from 'wagmi/chains'
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
+    [baseSepolia.id]: http(),
+    [base.id]: http(),
   },
   ssr: true,
 })
